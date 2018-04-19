06bfa786bfd4fa929dbf55fae0fa4d22901994b8
Dibbo <DeflatedPickle@gmail.com>
2018-04-19 15:46:58 +0100

    Improved movement through raycasting

--- Psionide/Assets/Scripts/PlayerMovementScript.cs
+++ Psionide/Assets/Scripts/PlayerMovementScript.cs
@@ -9,16 +9,22 @@ public class PlayerMovementScript : MonoBehaviour {
 	private bool _isMoving = false;
 	private Vector3 _originalPositon;
 	private Vector3 _newPosition;
+	private Vector3 _direction;
 
+	private Rigidbody2D _rigidbody2D;
 	private CameraShakeScript _cameraShake;
 	
-	private bool hasCollided = false;
+	// private bool hasCollided = false;
 
 	private void Awake() {
+		_rigidbody2D = GetComponent<Rigidbody2D>();
+		
 		_cameraShake = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraShakeScript>();
 	}
 
 	void Update () {
+		// Debug.DrawRay(_originalPositon, _newPosition, Color.red);
+		
 		var step = Speed * Time.deltaTime;
 		
 		_originalPositon = transform.position;
@@ -32,16 +38,23 @@ public class PlayerMovementScript : MonoBehaviour {
 
 			if (mainFinger.phase == TouchPhase.Began) {
 				// _newPosition = mainFinger.position;
-				_newPosition = RayTraceScript.RayHitPoint(mainFinger.position);
+				// _newPosition = RaycastScript.RayHitPoint(mainFinger.position);
 			}
 		}
 		else if (Input.GetMouseButtonDown(0)) {
 			_isMoving = true;
 
-			// _newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+			// _newPosition = mousePosition;
 			// _newPosition.z = 0f;
+
+			var cameraCenter = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.nearClipPlane));
+
+			var angle = Vector3.Angle(cameraCenter, mousePosition);
+			_direction = new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle));
 			
-			_newPosition = RayTraceScript.RayHitPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+			_newPosition = RaycastScript.RayHitPoint(mousePosition, _direction);
 		}
 		
 		if (_isMoving) {
@@ -52,17 +65,18 @@ public class PlayerMovementScript : MonoBehaviour {
 				_isMoving = false;
 			}*/
 			
-			if (hasCollided) {
+/*			if (hasCollided) {
 				_cameraShake.Shake(x: 0.2f, y: 0.2f);
 				_isMoving = false;
 				hasCollided = false;
-			}
+			}*/
 		}
 	}
 	
 	void OnCollisionEnter2D(Collision2D other) {
 		if (other.gameObject.CompareTag("Wall")) {
-			hasCollided = true;
+			_cameraShake.Shake(x: 0.2f, y: 0.2f);
+			_isMoving = false;
 		}
 	}
 }
---
+++ Psionide/Assets/Scripts/RaycastScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastScript : MonoBehaviour {
+	public static Vector3 RayHitPoint(Vector3 position, Vector3 direction) {
+		var hit = Physics2D.Raycast(position, direction);
+
+		if (hit.collider != null) {
+			return hit.collider.transform.position;
+		}
+        
+		return new Vector3(0, 0, 0);
+	}
+}
