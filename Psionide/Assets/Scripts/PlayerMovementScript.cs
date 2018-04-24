6ddf0b8fb3ce7f20abce3fba822c2f67c79ced4c
Dibbo <DeflatedPickle@gmail.com>
2018-04-24 14:09:56 +0100

    Fixed player movement

--- Psionide/Assets/Scripts/PlayerMovementScript.cs
+++ Psionide/Assets/Scripts/PlayerMovementScript.cs
@@ -9,12 +9,11 @@ public class PlayerMovementScript : MonoBehaviour {
 	private bool _isMoving = false;
 	private Vector3 _originalPositon;
 	private Vector3 _newPosition;
-	private Vector3 _direction;
+	private Vector3 _oldDirection;
+	private Vector3 _newDirection;
 
 	private Rigidbody2D _rigidbody2D;
 	private CameraShakeScript _cameraShake;
-	
-	// private bool hasCollided = false;
 
 	private void Awake() {
 		_rigidbody2D = GetComponent<Rigidbody2D>();
@@ -23,58 +22,54 @@ public class PlayerMovementScript : MonoBehaviour {
 	}
 
 	void Update () {
-		// Debug.DrawRay(_originalPositon, _newPosition, Color.red);
+		Debug.DrawRay(_originalPositon, _newPosition, Color.red);
+		// Debug.Log(_isMoving);
 		
 		var step = Speed * Time.deltaTime;
-		
 		_originalPositon = transform.position;
-		
 		var touches = Input.touches;
 
-		if (touches.Length == 1) {
-			_isMoving = true;
-			
-			var mainFinger = touches[0];
+		if (!_isMoving) {
+			if (touches.Length == 1) {
+				_isMoving = true;
+				var mainFinger = touches[0];
 
-			if (mainFinger.phase == TouchPhase.Began) {
-				// _newPosition = mainFinger.position;
-				// _newPosition = RaycastScript.RayHitPoint(mainFinger.position);
+				if (mainFinger.phase == TouchPhase.Began) {
+					// _newPosition = mainFinger.position;
+					// _newPosition = RaycastScript.RayHitPoint(mainFinger.position);
+				}
 			}
-		}
-		else if (Input.GetMouseButtonDown(0)) {
-			_isMoving = true;
+			else if (Input.GetMouseButtonDown(0)) {
+				var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				
+				var cameraCenter = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.nearClipPlane));
+				var angle = Vector3.Angle(cameraCenter, mousePosition);
+				_newDirection = new Vector3(Mathf.Sin(angle), 0, 0);
 
-			var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				if (_oldDirection.x >= 0.1 && _newDirection.x >= 0.1 || _oldDirection.x <= -0.1 && _newDirection.x <= -0.1) {
+					Debug.Log("Same direction!");
+					_newDirection = new Vector3(-Mathf.Sin(angle), 0, 0);
+				}
 
-			// _newPosition = mousePosition;
-			// _newPosition.z = 0f;
-
-			var cameraCenter = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.nearClipPlane));
-
-			var angle = Vector3.Angle(cameraCenter, mousePosition);
-			_direction = new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle));
+				_oldDirection = _newDirection;
+				Debug.Log(angle);
+				Debug.Log(_newDirection);
 			
-			_newPosition = RaycastScript.RayHitPoint(mousePosition, _direction);
+				_newPosition = RaycastScript.RayHitPoint(mousePosition, _newDirection);
+				
+				_isMoving = true;
+			}
 		}
 		
 		if (_isMoving) {
 			transform.position = Vector3.MoveTowards(transform.position, _newPosition, step);
-
-/*			if (transform.position == _newPosition) {
-				_cameraShake.Shake(x: 0.2f, y: 0.2f);
-				_isMoving = false;
-			}*/
-			
-/*			if (hasCollided) {
-				_cameraShake.Shake(x: 0.2f, y: 0.2f);
-				_isMoving = false;
-				hasCollided = false;
-			}*/
 		}
 	}
 	
 	void OnCollisionEnter2D(Collision2D other) {
 		if (other.gameObject.CompareTag("Wall")) {
+			Debug.Log("Hit a wall!");
+			
 			_cameraShake.Shake(x: 0.2f, y: 0.2f);
 			_isMoving = false;
 		}
