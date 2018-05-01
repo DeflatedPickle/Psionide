f77253fec9e3ade3f5e58df81f0deea5c0d036c7
Dibbo <DeflatedPickle@gmail.com>
2018-05-01 11:55:15 +0100

    Added a counter class

---
+++ Psionide/Assets/Scripts/Counter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Counter : MonoBehaviour {
+	public static float Max = 60f;
+	public float Step = 1f;
+
+	public bool Stopped = false;
+	public string CountDirection = "down";
+
+	private float _current;
+
+	public Counter(float max, float step, string direction) {
+		Max = max;
+		Step = step;
+		CountDirection = direction;
+
+		switch (CountDirection) {
+			case "down":
+				_current = Max;
+				break;
+				
+			case "up":
+				_current = 0;
+				break;
+		}
+	}
+
+	void Update () {
+		Debug.Log(_current);
+
+		switch (CountDirection) {
+			case "down": 
+				if (_current > 0) {
+					_current -= Step;
+				}
+				else {
+					Stopped = true;
+				}
+				break;
+				
+			case "up":
+				if (_current < 0) {
+					_current += Step;
+				}
+				else {
+					Stopped = true;
+				}
+				break;
+		}
+	}
+}
