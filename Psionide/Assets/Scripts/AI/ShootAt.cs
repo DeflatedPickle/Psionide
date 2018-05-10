a56f9d1d3dbe590f8598d39d2a461db1954ffee2
Dibbo <DeflatedPickle@gmail.com>
2018-05-10 11:37:36 +0100

    Added a ShootAt script

---
+++ Psionide/Assets/Scripts/AI/ShootAt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootAt : MonoBehaviour {
+    public Transform Target;
+    public float Inaccuracy;
+    public float Interval;
+
+    private Counter _intervalCounter;
+
+    private void Start() {
+        _intervalCounter = new Counter(Interval);
+    }
+
+    private void Update() {
+        _intervalCounter.Update();
+
+        if (_intervalCounter.Value <= 0) {
+            Debug.Log("Shooting");
+            _intervalCounter.Reset();
+        }
+    }
+}
