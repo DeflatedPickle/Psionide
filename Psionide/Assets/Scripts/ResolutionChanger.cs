c319a3a07ce565e32e214248e37d263af3c4e4d2
DeflatedPickle <DeflatedPickle@gmail.com>
2018-05-11 13:51:02 +0100

    Updated menu and added a ResolutionChanger script

---
+++ Psionide/Assets/Scripts/ResolutionChanger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionChanger : MonoBehaviour {
+	public float Resolution;
+	public ScreenOrientation ScreenOrientation;
+	
+	void Start () {
+		Camera.main.aspect = Resolution;
+		Screen.orientation = ScreenOrientation;
+	}
+}
