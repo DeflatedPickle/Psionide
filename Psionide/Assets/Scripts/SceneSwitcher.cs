0e00d542bbaa98269a6c0c51170b69e30d12aade
DeflatedPickle <DeflatedPickle@gmail.com>
2018-05-11 07:54:42 +0100

    Added canvas scaling and a scene switcher

---
+++ Psionide/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour {
+	public void LoadScene(string scene) {
+		SceneManager.LoadScene(scene);
+	}
+
+	public void Quit() {
+		Application.Quit();
+	}
+}
