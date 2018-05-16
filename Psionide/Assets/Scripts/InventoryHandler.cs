a280e4a8a483c13157cc819ce02caff57dcee135
Dibbo <DeflatedPickle@gmail.com>
2018-05-16 22:36:20 +0100

    Added an inventory

---
+++ Psionide/Assets/Scripts/InventoryHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryHandler : MonoBehaviour {
+	public List<GameObject> ComponentList = new List<GameObject>();
+	
+	public Transform InventoryGrid;
+
+	private List<Transform> _inventorySlots = new List<Transform>();
+	private List<GameObject> _inventoryContents = new List<GameObject>();
+
+	private void Awake() {
+		InventoryGrid = GameObject.Find("PlayerPrefab").transform.Find("Grid").transform;
+	}
+
+	void Start () {
+		foreach (var child in InventoryGrid.GetComponentsInChildren<Transform>()) {
+			if (child.name != "Grid") {
+				_inventorySlots.Add(child);
+			}
+		}
+	}
+
+	public bool IsSlotFree(int slot) {
+		try {
+			var temp = _inventorySlots[slot];
+			return true;
+		}
+		catch (ArgumentOutOfRangeException e) {
+			// Console.WriteLine(e);
+			return false;
+		}
+	}
+
+	public void AddItem(GameObject item) {
+		Debug.Log(string.Format("Picked Up: {0}", item));
+		
+		var num = 0;
+		while (!IsSlotFree(num)) {
+			num += 1;
+		}
+
+		var position = _inventorySlots[num].position;
+		Instantiate(item, InventoryGrid.Find(_inventorySlots[num].name));
+		
+		_inventoryContents.Add(item);
+	}
+}
---
+++ Psionide/Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour {
+	public GameObject Item;
+
+	private InventoryHandler _inventoryHandler;
+
+	private void Awake() {
+		_inventoryHandler = GameObject.FindWithTag("Player").transform.GetComponent<InventoryHandler>();
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		// Debug.Log(string.Format("Item collided with: {0}", other.name));
+		if (other.transform.CompareTag("Player")) {
+			_inventoryHandler.AddItem(Item);
+			Destroy(gameObject);
+		}
+	}
+}
