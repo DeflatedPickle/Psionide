dc28cd12b8559a49e2428b622a1d19b8f1ea9bb9
Dibble <DeflatedPickle@gmail.com>
2018-05-17 01:57:29 +0100

    Fixed InventoryHandler

--- Psionide/Assets/Scripts/InventoryHandler.cs
+++ Psionide/Assets/Scripts/InventoryHandler.cs
@@ -11,6 +11,8 @@ public class InventoryHandler : MonoBehaviour {
 	private List<Transform> _inventorySlots = new List<Transform>(23);
 	private List<GameObject> _inventoryContents = new List<GameObject>(23);
 
+	private int _freeSlot = 0;
+
 	private void Awake() {
 		InventoryGrid = GameObject.Find("PlayerPrefab").transform.Find("Grid").transform;
 	}
@@ -19,41 +21,19 @@ public class InventoryHandler : MonoBehaviour {
 		foreach (var child in InventoryGrid.GetComponentsInChildren<Transform>()) {
 			if (child.name != "Grid") {
 				_inventorySlots.Add(child);
+				_inventoryContents.Add(null);
 			}
 		}
-
-		for (var i = 0; i < 23; i++) {
-			_inventoryContents.Add(null);
-		}
-	}
-
-	public bool IsSlotFree(int slot) {
-		// Debug.Log(_inventoryContents[slot]);
-		var items = "";
-		foreach (var i in _inventoryContents) {
-			items += i + ", ";
-		}
-		Debug.Log(string.Format("Items: {0}", items));
-		
-		if (_inventoryContents[slot] == null) {
-			return true;
-		}
-
-		return false;
 	}
 
 	public void AddItem(GameObject item) {
 		Debug.Log(string.Format("Picked Up: {0}", item));
-		
-		var num = 0;
-		while (!IsSlotFree(num)) {
-			num ++;
-		}
-		Debug.Log(num);
 
-		var itemObject = Instantiate(item, InventoryGrid.Find(_inventorySlots[num].name));
-		itemObject.transform.position = _inventorySlots[num].position;
+		var itemObject = Instantiate(item, InventoryGrid.Find(_inventorySlots[_freeSlot].name));
+		itemObject.transform.position = _inventorySlots[_freeSlot].position;
 		
-		_inventoryContents[num] = item.gameObject;
+		_inventoryContents[_freeSlot] = item.gameObject;
+
+		_freeSlot++;
 	}
 }
